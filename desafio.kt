7faa0a580dd025b338fd1eb1571131f25b8e8804
Tomé Lopes Carlos Camueji <lopezezareno6@gmail.com>
2025-07-20 20:23:57 -0300

    Ajuste nos valores desafio DIO

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,66 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+data class Aluno (val nome: String)
+data class ConteudoEducacional(val nome:String)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+class Formacao(val nome: String, val nivel: String, val conteudos: List<ConteudoEducacional>){
+    val alunosMatriculados = mutableListOf<Aluno>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(aluno: Aluno) {
+        alunosMatriculados.add(aluno)
+        println("Aluno ${aluno.nome} matriculado na formação $nome.")
+    }
+    
+    fun matricular(alunos: List<Aluno>) {
+        alunosMatriculados.addAll(alunos)
+        println("${alunos.size} alunos matriculados na formação $nome.")
+    }
+    
+    fun listarAlunos(){
+        if(alunosMatriculados.isEmpty()){
+            println("Nenhum aluno matriculado nesta formação ainda.")
+        }else{
+            println("Alunos matriculados na formação $nome:")
+            alunosMatriculados.forEach { aluno ->
+                println("- ${aluno.nome}")
+            }
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // 1.Vou aqui Criar alguns conteúdos educacionais
+   val kotlinBasico = ConteudoEducacional("Introdução ao Kotlin")
+   val kotlinPOO = ConteudoEducacional("Programação Orientada a Objetos com Kotlin")
+   val kotlinFundamentos = ConteudoEducacional("Fundamentos de Desenvolvimento Android")
+    
+     val formacaoAndroid = Formacao(
+    nome = "Formação Desenvolvedor Android",
+    nivel = "Intermediário",
+    conteudos = listOf(kotlinBasico, kotlinPOO, kotlinFundamentos)
+    )
+    
+    println("Formação criada: ${formacaoAndroid.nome} - Nível: ${formacaoAndroid.nivel}")
+    println("Conteúdos da formação:")
+    formacaoAndroid.conteudos.forEach { conteudo ->
+        println("- ${conteudo.nome}")
+    }
+    println("---")
+    
+    // 3. Criar alguns alunos
+    val aluno1 = Aluno("João Silva")
+    val aluno2 = Aluno("Maria Souza")
+    val aluno3 = Aluno("Pedro Mendes")
+    
+    
+    // 4. Matricular alunos na formação
+    formacaoAndroid.matricular(aluno1)
+    formacaoAndroid.matricular(aluno2)
+    
+    println("---\n")
+    formacaoAndroid.listarAlunos()
+    
+    println("---\n")
+    formacaoAndroid.matricular(listOf(aluno3))
+    
+    println("---\n")
+    formacaoAndroid.listarAlunos() 
 }
